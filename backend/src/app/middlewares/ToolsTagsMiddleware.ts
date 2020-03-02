09bbce2d32d46ca9f049be96524998a0713f6b49
gustavolwalker <gustavolwalker@gmail.com>
2020-03-02 11:19:46 -0300

    Add path validator to middleware

--- backend/src/app/middlewares/ToolsTagsMiddleware.ts
+++ backend/src/app/middlewares/ToolsTagsMiddleware.ts
@@ -2,12 +2,14 @@ import { Request, Response, NextFunction } from "express";
 import { Tool } from "../entities/Tool";
 import { Tag } from "../entities/Tag";
 
-export function ToolsTagsMiddleware(req: Request, res: Response, next: NextFunction) {
-    const entity: Tool = req.body;
-    if (entity?.tags?.length) {
-        entity.tags = entity.tags.map((tag: Tag | string) =>
-            (tag instanceof Tag ? tag : { tag })
-        );
+export const ToolsTagsMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    if (req.path.includes('/tools')) {
+        const entity: Tool = req.body;
+        if (entity?.tags?.length) {
+            entity.tags = entity.tags.map((tag: Tag | string) =>
+                (tag instanceof Tag ? tag : { tag })
+            );
+        }
     }
     return next();
 }
