3d02b1a88eee0fe9227dc9f2ccc10f8763f9b1e1
gustavolwalker <gustavolwalker@gmail.com>
2020-03-24 11:05:22 -0300

    added function to transform every tool link in a real outside links

--- frontend/src/components/tool.tsx
+++ frontend/src/components/tool.tsx
@@ -7,27 +7,33 @@ type Props = {
     handleRemove: (tool: ITool) => void
 }
 
-const Tool: React.FC<Props> = ({ tool, handleRemove }) => (
-    <div className="card elevation1">
-        <div className="pure-g">
-            <div className="pure-u-21-24">
-                <a href={tool.link} target="_blank" rel="noopener noreferrer">{tool.title}</a>
-            </div>
-            <div className="pure-u-3-24 center">
-                <button type="button" className="button-xsmall pure-button" role="link" onClick={() => handleRemove(tool)} hidden={!isAuthenticated()}>X remove</button>
-            </div>
-        </div>
-        <div className="pure-g">
-            <div className="pure-u-1">
-                {tool.description}
-            </div>
-        </div>
-        <div className="pure-g">
-            <div className="pure-u-1">
-                <b>{tool.tags.map(tag => "#".concat(tag)).join(" ")}</b>
-            </div>
-        </div>
-    </div >
-);
+const Tool: React.FC<Props> = ({ tool, handleRemove }) => {
 
+    const normalizeLink = (link:string) =>{
+        return link.includes("//") ? link : "//".concat(link);
+    }
+
+    return (
+        <div className="card elevation1">
+            <div className="pure-g">
+                <div className="pure-u-21-24">
+                    <a href={normalizeLink(tool.link)} target="_blank" rel="noopener noreferrer">{tool.title}</a>
+                </div>
+                <div className="pure-u-3-24 center">
+                    <button type="button" className="button-xsmall pure-button" role="link" onClick={() => handleRemove(tool)} hidden={!isAuthenticated()}>X remove</button>
+                </div>
+            </div>
+            <div className="pure-g">
+                <div className="pure-u-1">
+                    {tool.description}
+                </div>
+            </div>
+            <div className="pure-g">
+                <div className="pure-u-1">
+                    <b>{tool.tags.map(tag => "#".concat(tag)).join(" ")}</b>
+                </div>
+            </div>
+        </div >
+    );
+};
 export default Tool;
