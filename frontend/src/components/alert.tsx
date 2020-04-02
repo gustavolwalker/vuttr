e08777d3e203d8b98c9d03e2c2dfcde2f91bd9b4
gustavolwalker <gustavolwalker@gmail.com>
2020-04-02 00:36:28 -0300

    added icon in alert messages

--- frontend/src/components/alert.tsx
+++ frontend/src/components/alert.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 
+import { ReactComponent as Info } from '../assets/info.svg';
+import { ReactComponent as Success } from '../assets/success.svg';
+import { ReactComponent as Error } from '../assets/error.svg';
+import { ReactComponent as Warning } from '../assets/warning.svg';
+
 type Props = {
     message?: string,
-    type: 'info' | 'error' | 'warnning';
+    type: 'info' | 'success' | 'error' | 'warnning';
 }
 
 const Alert: React.FC<Props> = ({ message, type }) => (
-    message ?
-        <div className={"alert " + type} > {message}</div>
-        : <></>
+    <>
+        {message && (type === 'info' || !type) &&
+            <div className="alert info" >
+                <Info className="icon" /> {message}
+            </div>}
+        {message && type === 'success' &&
+            <div className="alert success" >
+                <Success className="icon" /> {message}
+            </div>}
+        {message && type === 'error' &&
+            <div className="alert error" >
+                <Error className="icon" /> {message}
+            </div>}
+        {message && type === 'warnning' &&
+            <div className="alert warnning" >
+                <Warning className="icon" /> {message}
+            </div>}
+    </>
 );
 
 export default Alert;
